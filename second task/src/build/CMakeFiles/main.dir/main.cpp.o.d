b635970c27b764f9e4eac2eec65a9e6bac96b941
Akhmat Taumurzaev <akhmat.taumurzaev@yandex.ru>
2024-03-27 20:54:05 +0300

    Uploading files

---
+++ second task/src/build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,261 @@
+CMakeFiles/main.dir/main.cpp.o: \
+ /home/a.taumurzaev/Desktop/SVT/2/src/main.cpp /usr/include/stdc-predef.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_common.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/build/inmost_options.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/string \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/c++config.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/pstl/pstl_config.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stringfwd.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/memoryfwd.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/char_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/postypes.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cwchar \
+ /usr/include/wchar.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stddef.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stdarg.h \
+ /usr/include/bits/wchar.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/type_traits \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cstdint \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/types.h \
+ /usr/include/bits/timesize.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/stdint-intn.h \
+ /usr/include/bits/stdint-uintn.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/allocator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/c++allocator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/new_allocator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/new \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/exception.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/functexcept.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/exception_defines.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/move.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/cpp_type_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/localefwd.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/c++locale.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/clocale \
+ /usr/include/locale.h /usr/include/bits/locale.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/iosfwd \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cctype \
+ /usr/include/ctype.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/ostream_insert.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/cxxabi_forced.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_iterator_base_types.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_iterator_base_funcs.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/concept_check.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/debug/assertions.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_iterator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/type_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/ptr_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_function.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/backward/binders.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/numeric_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_algobase.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_pair.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/utility.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/debug/debug.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/predefined_ops.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/refwrap.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/invoke.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/range_access.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/initializer_list \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/basic_string.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/alloc_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/alloc_traits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_construct.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/string_view \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/functional_hash.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/hash_bytes.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/string_view.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/string_conversions.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/std_abs.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cstdio \
+ /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
+ /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cerrno \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/bits/types/error_t.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/charconv.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/basic_string.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/vector \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_uninitialized.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_vector.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_bvector.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/vector.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/set \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_tree.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/aligned_buffer.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/node_handle.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_set.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_multiset.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/erase_if.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/map \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_map.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tuple \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/uses_allocator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_multimap.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/list \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_list.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/allocated_ptr.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/list.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/algorithm \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_algo.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/algorithmfwd.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_heap.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_tempbuf.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/uniform_int_dist.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/pstl/glue_algorithm_defs.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/pstl/execution_defs.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cmath \
+ /usr/include/math.h /usr/include/bits/math-vector.h \
+ /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/specfun.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/limits \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/gamma.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/special_function_util.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/bessel_function.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/beta_function.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/ell_integral.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/exp_integral.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/hypergeometric.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/legendre_function.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/modified_bessel_func.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/poly_hermite.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/poly_laguerre.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/tr1/riemann_zeta.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/iostream \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ostream \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ios \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/exception \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/exception_ptr.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/cxxabi_init_exception.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/typeinfo \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/nested_exception.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/ios_base.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/atomicity.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/gthr.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_itimerspec.h /usr/include/bits/setjmp.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/atomic_word.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_classes.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_classes.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/system_error \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/error_constants.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/stdexcept \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/streambuf \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/streambuf.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/basic_ios.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_facets.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cwctype \
+ /usr/include/wctype.h /usr/include/bits/wctype-wchar.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/ctype_base.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/streambuf_iterator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/ctype_inline.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_facets.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/basic_ios.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/ostream.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/istream \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/istream.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/fstream \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/codecvt.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/basic_file.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/c++io.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/fstream.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/stdlib.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include-fixed/limits.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/uio_lim.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/container.hpp \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cstddef \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cstring \
+ /usr/include/string.h /usr/include/strings.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cassert \
+ /usr/include/assert.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/memory \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stl_raw_storage_iter.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/align.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bit \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/unique_ptr.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/shared_ptr.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/shared_ptr_base.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ext/concurrence.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/shared_ptr_atomic.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/atomic_base.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/atomic_lockfree_defines.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/backward/auto_ptr.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/pstl/glue_memory_defs.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/iterator \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/stream_iterator.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/cfloat \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/float.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_mesh.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_data.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_expression.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_sparse.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/sstream \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/sstream.tcc \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_dense.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/iomanip \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/locale \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_facets_nonio.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/ctime \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/time_members.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/messages_members.h \
+ /usr/include/libintl.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_facets_nonio.tcc \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/locale_conv.h \
+ /opt/ohpc/pub/compiler/gcc/12.2.0/include/c++/12.2.0/bits/quoted_string.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_solver.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_partitioner.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_variable.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_autodiff.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_block_variable.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_nonlinear.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_residual.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_operator.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_model.h \
+ /home/a.taumurzaev/Desktop/SVT/2/INMOST/Source/Headers/inmost_xml.h
